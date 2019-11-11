e57490b4cd80ce651c928bdc8bcb3bee083aeea7
Meithal <hurin8888@gmail.com>
2019-11-11 14:23:43 +0100

    Add simple ad-hoc templating

--- __main__.py
+++ __main__.py
@@ -1,8 +1,25 @@
-import aiohttp
-import migrate.versioning.api as migrate_api
-import pathlib
-import os
+import logging
 
-# print(os.getcwd())
+from aiohttp import web as aiow
 
-migrate_api.create(pathlib.Path('.') / 'migrations', "Warcraft 3 Vault")
+import templates
+
+routes = aiow.RouteTableDef()
+
+
+@routes.get('/')
+async def handle(request):
+    name = request.match_info.get('name', "Anonymous")
+    text = "Hello, " + name
+    return aiow.Response(text=templates.parse('site', 'toto', title='Foo', foo='bar'))  # tpls['site']['common'])
+
+
+def main():
+    app = aiow.Application()
+    logging.basicConfig(level=logging.DEBUG)
+    app.add_routes(routes)
+    aiow.run_app(app)
+
+
+if __name__ == '__main__':
+    main()
---
+++ templates/__init__.py
@@ -0,0 +1,65 @@
+import os
+import pathlib
+
+
+def get_templates():
+    templates = {}
+    with pathlib.Path('.') / 'templates' as tplpath:
+        for cat in {*os.listdir(tplpath)} - {'__init__.py', '__pycache__'}:
+            with tplpath / cat as catpath:
+                for tpl in (t for t in os.listdir(catpath) if t.endswith('.html')):
+                    templates[cat, tpl.split('.')[0]] = open(catpath / tpl).read()
+
+    return templates
+
+
+tpls = get_templates()
+expanded = {}
+
+
+def get_template(cat, tpl):
+    return tpls[cat, tpl]
+
+
+def recurse_replace(kwargs, domain, name):
+    recur = RecurReplacementHandler(kwargs, get_template(domain, name))
+    ct = get_template(domain, name).format_map(recur)
+    return ct
+
+
+class RecurReplacementHandler(dict):
+
+    def __init__(self, initial_values, content):
+        super().__init__(initial_values)
+
+        self.initial_values = initial_values
+        self.content = self.trunced = content
+
+    def __missing__(self, key):
+
+        if key.startswith("export_"):
+            _, domain, name, key = key.split("_")
+            self.trunced = self.content.replace(f"{{export_{domain}_{name}_{key}}}", "")
+            self.initial_values[key] = self.trunced
+            self.initial_values['$to_remove'] += len(self.trunced)
+
+            return recurse_replace(self.initial_values, domain, name)
+        else:
+            return f"{{{key}}}"
+
+    def __getitem__(self, item):
+        if item not in self:
+            return self.__missing__(item)
+        return f"{{{item}}}"
+
+
+class FinalReplacerHandler(dict):
+    def __missing__(self, key):
+        return f"<<<missing {key} >>>"
+
+
+def parse(domain, name, **kwargs):
+    kwargs['$to_remove'] = 0
+    inter = recurse_replace(kwargs, domain, name)[:-kwargs['$to_remove']]
+    final = inter.format_map(FinalReplacerHandler(kwargs)).format_map(FinalReplacerHandler(kwargs))
+    return final
