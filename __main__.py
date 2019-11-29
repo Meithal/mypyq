db2624eabdbace4a5e6f744b288966cf34de29cc
Meithal <hurin8888@gmail.com>
2019-11-29 06:35:46 +0100

    Serve compiled files

--- __main__.py
+++ __main__.py
@@ -13,25 +13,28 @@ rp = pathlib.Path('.')
 
 
 async def listen_to_sass_changes(app):
+    def _compile():
+        try:
+            sass.compile(dirname=(rp / 'scss', rp / '_css'), output_style='expanded')
+        except Exception as e:
+            print("sass compilation failed", e)
+
     print("sass listener started")
+    _compile()
     sassfiles : typing.Dict[pathlib.Path, os.stat_result] = {
         f: 0
-        for f in ((rp / 'scss').iterdir())
+        for f in (rp / 'scss').iterdir()
     }
     for f in sassfiles:
         sassfiles[f] = f.stat()
-    #
     while True:
         await asyncio.sleep(2.0)
         for f in sassfiles:
             stats = f.stat()
             if sassfiles[f].st_mtime < stats.st_mtime:
                 print(f, "has changed, recompiling...")
-                try:
-                    sass.compile(dirname=(rp / 'scss', rp / '_css'), output_style='expanded')
-                    sassfiles[f] = stats
-                except Exception as e:
-                    print("sass compilation failed", e)
+                _compile()
+                sassfiles[f] = stats
 
 
 async def start_sass_listener(app):
@@ -45,6 +48,8 @@ routes = aiow.RouteTableDef()
 async def handle(request):
     name = request.match_info.get('name', "Anonymous")
     return aiow.Response(text=templates.parse('site', 'toto', title='Foo', foo='bar'), headers={'Content-Type': 'text/html'})  # tpls['site']['common'])
+
+routes.static('/_css', rp / '_css')
 
 
 def main():
